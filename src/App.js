77b867fdef1bd471dbcf0da177e3920ff1419300
Werbyston Lemos <werbyston@hotmail.com>
2026-07-13 22:50:52 -0300

    [Feature] sessão de Calling desenvolvida

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './parts/Header'
 import Banner from './parts/Banner';
+import Calling from './parts/Calling';
 
 function App() {
   return (
     <div className="mainContainer">
       <Header />
       <Banner />
+      <Calling />
     </div>
   );
 }
