7a8bd82521c2595a7da188cb6dc5f064afd95815
Werbyston Lemos <werbyston@hotmail.com>
2026-07-15 16:11:08 -0300

    [Feature] section Ready criada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './parts/Header'
 import Banner from './parts/Banner'
 import Calling from './parts/Calling'
 import Processing from './parts/Processings';
+import Ready from './parts/Ready';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <Calling />
       <Processing />
+      <Ready />
     </div>
   );
 }
