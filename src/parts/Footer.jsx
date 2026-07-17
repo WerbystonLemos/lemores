ae126166369740076235c0c90c8c254764ca1800
Werbyston Lemos <werbyston@hotmail.com>
2026-07-17 18:28:56 -0300

    [Fix] correção de responsividade

--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -1,17 +1,16 @@
-import './../assets/css/footer.css'
-import logomarca from '../assets/images/logomarca.png'
+import logomarca from '../assets/images/logomarcaBCO.png'
 
 export default function Footer()
 {
     return(
-        <div className="containerFooter">
+        <div className="flex items-start justify-between px-5 py-8 flex-col gap-8">
 
-            <div className="footerItem">
-                <img src={logomarca} alt="Lemores" />
-                <p>
+            <div className="flex items-start justify-center flex-col gap-5 py-16">
+                <img className='w-48' src={logomarca} alt="Lemores" />
+                <p className='text-[18pt] text-white'>
                     Transforme ideias em sites profissionais que geram resultados e impulsiona negócios.
                 </p>
-                <div className="footerItemRS">
+                <div className="flex items-center justify-arround text-white w-full gap-5">
                     <span>In</span>
                     <span>fb</span>
                     <span>Ln</span>
@@ -19,35 +18,41 @@ export default function Footer()
                 </div>
             </div>
 
-            <div className="footerItem">
-                <h1>Navegação</h1>
-                <ul>
-                    <li>Início</li>
-                    <li>Serviços</li>
-                    <li>Empresa</li>
-                    <li>Planos</li>
-                    <li>Contato</li>
+            <div className=' flex justify-between items-start gap-8'>
+                <div className="flex items-start justify-center flex-col">
+                    <h1 className='text-[1.5em] text-white'>Navegação</h1>
+                    <ul className='text-white no-underline list-none'>
+                        <li className=''>Início</li>
+                        <li className=''>Serviços</li>
+                        <li className=''>Empresa</li>
+                        <li className=''>Planos</li>
+                        <li className=''>Contato</li>
+                    </ul>
+                </div>
+                
+                <div className="flex items-start justify-center flex-col">
+                    <h1 className='text-[1.5em] text-white'>Serviços</h1>
+                    <ul className='text-white no-underline list-none'>
+                        <li className=''>Sites Institucionais</li>
+                        <li className=''>Landing Page</li>
+                        <li className=''>Manutenção de Sites</li>
+                        <li className=''>Otimização (SEO)</li>
+                    </ul>
+                </div>
+            </div>   
+            
+            <div className="flex items-center justify-center flex-col">
+                <h1 className='text-[1.5em] text-white'>Contato</h1>
+                <ul className='text-white no-underline list-none text-center'>
+                    <li className=''>(88) 98888 8888</li>
+                    <li className=''>email@email.com</li>
+                    <li className=''>Atendimento comercial: Seg-Sex | 8h às 18h</li>
+                    <li className=''>Suporte: 24h por dia</li>
                 </ul>
             </div>
-            
-            <div className="footerItem">
-                <h1>Serviços</h1>
-                <ul>
-                    <li>Sites Institucionais</li>
-                    <li>Landing Page</li>
-                    <li>Manutenção de Sites</li>
-                    <li>Otimização (SEO)</li>
-                </ul>
-            </div>
-            
-            <div className="footerItem">
-                <h1>Contato</h1>
-                <ul>
-                    <li>(88) 98888 8888</li>
-                    <li>email@email.com</li>
-                    <li>Atendimento comercial: Seg-Sex | 8h às 18h</li>
-                    <li>Suporte: 24h por dia</li>
-                </ul>
+
+            <div>
+                <p className='text-center text-terceary text-sm'>&copy;Todos direitos reservado - Lemores Sistemas 2026</p>
             </div>
             
         </div>
--- src/parts/Ready.jsx
+++ src/parts/Ready.jsx
@@ -1,19 +1,17 @@
-import './../assets/css/ready.css'
-
 export default function Ready()
 {
     return(
-        <div className="containerReady">
-            <div id="ready">
+        <div className="flex items-center justify-center p-5">
+            <div className='flex flex-col items-center justify-center gap-5 bg-terceary p-7 rounded rounded-lg'>
                     
-                <div>
+                <div className='text-secondary'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
                     </svg>
                 </div>
 
-                <div>
-                    <span>Pronto para ter um site que traz resultados?</span>
+                <div className='text-white flex flex-col gap-3'>
+                    <span className='text-[1.8em]'>Pronto para ter um site que traz resultados?</span>
                     <span>Fale com a gente e receba uma proposta personalizada para o seu negócio.</span>
                 </div>
                 
