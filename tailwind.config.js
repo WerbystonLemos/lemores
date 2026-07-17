506ad7ae1cee1a004b2787ff4192b6fdddb63c9e
Werbyston Lemos <werbyston@hotmail.com>
2026-07-17 17:49:49 -0300

    [Fix] correções de resonsividade

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: 
+      {
+        primary: '#091420',
+        secondary: '#20A2F2',
+        terceary: '#13212c',
+        gray: '#D9DCE0',
+        white: '#FFF',
+      }
+    },
+  },
+  plugins: [],
+}
